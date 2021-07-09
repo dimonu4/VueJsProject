7e7c97675ec524882cbcf1013504b434f6304281
dimonu4 <dimonu4.1989@gmail.com>
2021-07-09 18:07:12 +0300

    Homework-3

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,122 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :class="[$style.wrapper]">
+   <header>
+     <h1>My personal costs</h1>
+   </header>
+   <main>
+     <div class="buttonAdd">
+       <button v-on:click="showClick">Add new Coast +</button>
+     </div>
+     <AddPayment v-show='isShow' v-on:addNewPayment="addData"/>
+     <br>
+     <PaymentsDisplay :list="paymentsList" :pages="pages"/>
+     <Pagination @paginationClick="changeList" :listLength="paymentsList.length"/>
+   </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import PaymentsDisplay from './components/PaymentsDisplay.vue'
+import AddPayment from "./components/AddPayment.vue"
+import Pagination from "./components/Pagination.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    PaymentsDisplay,
+    AddPayment,
+    Pagination,
+
+  },
+  data:()=>({
+    isShow:false,
+    paymentsList:[],
+    pages:1,
+  }),
+  methods:{
+        changeList(data){
+        console.log(data)
+        this.pages=data;
+      },
+    showClick(){
+      this.isShow=!this.isShow
+    },
+    addData(data){
+      this.paymentsList.push(data)
+    },
+    fetchData(){
+      return[
+        {
+          date:'28.03.2020',
+          category:'Food',
+          value:169
+        },
+        {
+          date:'28.03.2021',
+          category:'Sport',
+          value:400
+        },
+        {
+          date:'28.05.2020',
+          category:'Internet',
+          value:200
+        },
+                {
+          date:'28.03.2020',
+          category:'Food',
+          value:169
+        },
+        {
+          date:'28.03.2021',
+          category:'Sport',
+          value:400
+        },
+        {
+          date:'28.05.2020',
+          category:'Internet',
+          value:200
+        },
+                {
+          date:'28.03.2020',
+          category:'Food',
+          value:169
+        },
+        {
+          date:'28.03.2021',
+          category:'Sport',
+          value:400
+        },
+        {
+          date:'28.05.2020',
+          category:'Internet',
+          value:200
+        },
+                {
+          date:'28.03.2020',
+          category:'Food',
+          value:169
+        },
+        {
+          date:'28.03.2021',
+          category:'Sport',
+          value:400
+        },
+        {
+          date:'28.05.2020',
+          category:'Internet',
+          value:200
+        },
+      ]
+    },
+  },
+      created(){
+      this.paymentsList= this.fetchData()
+    }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" module>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +124,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.wrapper{
+  width:800px;
+  margin:0 auto;
+}
+</style>
+<style>
+.buttonAdd button{
+  background-color: rgb(112, 202, 112);
+  padding:1em;
+  color: white;
 }
 </style>
---
+++ src/components/Pagination.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <span>&#60;</span>
+        <span @click='clicked' v-for="n in Math.ceil(this.listLength/5)" :key="n">
+            {{n}}
+        </span>
+        <span>&#62;</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Pagination",
+    props:{
+        listLength:{
+        type:Number,
+        }
+    },
+    methods:{
+        clicked(event){
+            this.$emit("paginationClick", +event.target.outerText)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
